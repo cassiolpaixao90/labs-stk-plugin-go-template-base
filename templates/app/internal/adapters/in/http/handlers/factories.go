084cdeac8d5421fc1ccd0c38585735cfbb0c19a4
cassiolpaixao90 <cassiopaixaoti@gmail.com>
2023-11-22 09:12:14 -0300

    feat: update examples

--- templates/app/internal/adapters/in/http/handlers/factories.go
+++ templates/app/internal/adapters/in/http/handlers/factories.go
@@ -11,19 +11,3 @@ func NewGetAll{{resource|capitalize}}Instance() {{resource}}.IGetAll{{resource|c
 	service := services.NewGetAll{{resource|capitalize}}Service(client)
 	return {{resource}}.NewGetAll{{resource|capitalize}}Handler(service)
 }
-
-func NewCreate{{resource|capitalize}}Instance() {{resource}}.ICreate{{resource|capitalize}}Handler {
-	return {{resource}}.NewCreate{{resource|capitalize}}Handler()
-}
-
-func NewGet{{resource|capitalize}}ByIdInstance() {{resource}}.IGet{{resource|capitalize}}ByIdHandler {
-	return {{resource}}.NewGet{{resource|capitalize}}ByIdIdHandler()
-}
-
-func NewUpdate{{resource|capitalize}}ByIdInstance() {{resource}}.IUpdate{{resource|capitalize}}ByIdHandler {
-	return {{resource}}.NewUpdate{{resource|capitalize}}ByIdHandler()
-}
-
-func NewDelete{{resource|capitalize}}ByIdInstance() {{resource}}.IDelete{{resource|capitalize}}ByIdHandler {
-	return {{resource}}.NewDelete{{resource|capitalize}}ByIdHandler()
-}
--- templates/app/internal/adapters/in/http/routes.go
+++ templates/app/internal/adapters/in/http/routes.go
@@ -15,8 +15,4 @@ func NewHttpRoutes(e *echo.Echo) *Routes {
 
 func (r *Routes) SetupRouter() {
 	r.echo.GET("/{{resource}}", handlers.NewGetAll{{resource|capitalize}}Instance().GetAll{{resource|capitalize}})
-	r.echo.POST("/{{resource}}", handlers.NewCreate{{resource|capitalize}}Instance().Create{{resource|capitalize}})
-	r.echo.GET("/{{resource}}/:id", handlers.NewGet{{resource|capitalize}}ByIdInstance().Get{{resource|capitalize}}ById)
-	r.echo.PUT("/{{resource}}/:id", handlers.NewUpdate{{resource|capitalize}}ByIdInstance().Update{{resource|capitalize}}ById)
-	r.echo.DELETE("/{{resource}}/:id", handlers.NewDelete{{resource|capitalize}}ByIdInstance().Delete{{resource|capitalize}}ById)
 }
